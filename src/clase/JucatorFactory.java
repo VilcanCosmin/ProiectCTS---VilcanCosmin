62bc5e34e06ca9aebf3c3f124e3af24cb0ceede8
VilcanCosmin <vilcan.cosmin@gmail.com>
2015-05-25 23:32:15 +0300

    first

--- src/clase/JucatorFactory.java
+++ src/clase/JucatorFactory.java
@@ -8,11 +8,13 @@ public class JucatorFactory
 	{
 		switch(fotbalist)
 		{
+		
 		case Portar: return new Portar("Manuel Neuer", 26);
 		case Fundas: return new Fundas("Sergio Ramos", 28);
 		case Mijlocas: return new Mijlocas("Cristiano Ronaldo", 30);
 		case Atacant: return new Atacant("Lionel Messi", 28);
 		case Polivalent: return new Polivalent("Gheorghe Hagi", 38);
+		
 		}
 		
 		return null;
